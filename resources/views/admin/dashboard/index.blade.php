958b5a1d695738555b95a74388956011e41f8ab8
Shakeel Ahmad <shakeel2717@gmail.com>
2023-08-29 18:41:17 +0500

    dashboar stat update

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -40,6 +40,27 @@
                         <div
                             class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
                             <dl class="mb-0">
+                                <dt class="fs-3 fw-bold">{{ $countries->where('status',true)->count() }}</dt>
+                                <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Active Countries</dd>
+                            </dl>
+                            <div class="item item-rounded-lg bg-body-light">
+                                <i class="far fa-gem fs-3 text-primary"></i>
+                            </div>
+                        </div>
+                        <div class="bg-body-light rounded-bottom">
+                            <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between"
+                                href="javascript:void(0)">
+                                <span>View all Countries</span>
+                                <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xxl-3">
+                    <div class="block block-rounded d-flex flex-column h-100 mb-0">
+                        <div
+                            class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
+                            <dl class="mb-0">
                                 <dt class="fs-3 fw-bold">{{ $numbers->count() }}</dt>
                                 <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Total Numbers</dd>
                             </dl>
